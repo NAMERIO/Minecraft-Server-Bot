4611b42de9f6e7aa499ef75f8e5d8e8c4e1e437c
NAMERIO <natanberhanu22@gmail.com>
2025-12-24 01:57:22 -0600

    added trusted url for plugin

--- src/constants.js
+++ src/constants.js
@@ -1,18 +1,19 @@
 module.exports = {
-  COOLDOWN_MS: 300000, // 5 minutes
-  SHUTDOWN_RESET_MS: 120000, // 2 minutes
-  POLL_INTERVAL_MS: 60000, // 1 minute
-  RESTART_SAVE_DELAY_MS: 5000, // 5 seconds
-  RCON_TIMEOUT_MS: 10000, // 10 seconds
-  ALLOWED_DOMAINS: [
-    'modrinth.com',
-    'www.modrinth.com',
-    'spigotmc.org',
-    'www.spigotmc.org',
-    'papermc.io',
-    'hangar.papermc.io',
-    'www.papermc.io',
-    'www.hangar.papermc.io',
-    'mc.resurviv.biz',
-  ]
+    COOLDOWN_MS: 300000, // 5 minutes
+    SHUTDOWN_RESET_MS: 120000, // 2 minutes
+    POLL_INTERVAL_MS: 60000, // 1 minute
+    RESTART_SAVE_DELAY_MS: 5000, // 5 seconds
+    RCON_TIMEOUT_MS: 10000, // 10 seconds
+    ALLOWED_DOMAINS: [
+        'modrinth.com',
+        'www.modrinth.com',
+        'spigotmc.org',
+        'www.spigotmc.org',
+        'papermc.io',
+        'hangar.papermc.io',
+        'www.papermc.io',
+        'www.hangar.papermc.io',
+        'hangarcdn.papermc.io',
+        'mc.resurviv.biz',
+    ]
 };
